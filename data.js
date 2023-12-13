856f92666287f35556165345f552bfa257ff30a6
Zabooheeg <zaboodable@gmail.com>
2023-12-13 15:46:43 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+var data = {
+    "Information": {
+        "Quick Copy":{
+            "Webchat": {
+                "Main":[
+                    {
+                        "Button":true,
+                        "Title": "Hello, How can I help?",
+                        "Content": "Hello, thank you for contacting the Service Centre, how can I help?"
+                    }, 
+                    {
+                        "Button":true,
+                        "Title": "What is your Legacy District?",
+                        "Content": "Could I take the name of your Legacy District please?"
+                    }
+                ],
+                "Main 2":[
+                    {
+                        "Button":true,
+                        "Title": "Whaahahfoiusahfoiawhdfgfyusadfku?",
+                        "Content": "Could I take the name of your Legacy District please?"
+                    }
+                ]
+            },
+            "Links":{
+                "Link with comment": [
+                    {
+                        "Bold":true,
+                        "Content":"Loan Equip"
+                    },
+                    {
+                        "Button":true,
+                        "Title": "Loan Equipment",
+                        "Content": "To request Loan IT Equipment, please use the following form http://example.com"
+                    },
+                    {
+                        "Button":true,
+                        "Title": "Loan Laptop",
+                        "Content": "Please visit  http://example.com/laptop to request a laptop"
+                    }
+                ]
+            }
+        }
+    }
+}
